80412dc871044c08a9614fe3d554bc91cb59dfe3
Riley McCann <rmccann@udel.edu>
2018-04-09 13:54:41 -0400

    Update Direction

--- src/Direction.java
+++ src/Direction.java
@@ -1,26 +1,22 @@
+c enum Direction {
+
+	NORTH("north"),
+		NORTHEAST("northeast"),
+			EAST("east"),
+				SOUTHEAST("southeast"),
+					SOUTH("south"),
+						SOUTHWEST("southwest"),
+							WEST("west"),
+								NORTHWEST("northwest");
+									
+										private String name = null;
+											
+												private Direction(String s){
+														name = s;
+															}
+																public String getName() {
+																		return name;
+																			}
 
 
-
-public enum Direction {
-
-	HALT("idle_ewns"),
-	NORTH("forward_north"),
-	NORTHEAST("forward_northeast"),
-	EAST("forward_east"),
-	SOUTHEAST("forward_southeast"),
-	SOUTH("forward_south"),
-	SOUTHWEST("forward_southwest"),
-	WEST("forward_west"),
-	NORTHWEST("forward_northwest");
-	
-	private String name = null;
-	
-	private Direction(String s){
-		name = s;
-	}
-	public String getName() {
-		return name;
-	}
-
-
-}
+																			}
